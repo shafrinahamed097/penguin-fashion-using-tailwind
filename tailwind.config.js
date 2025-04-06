faca3e147b424a9557fb2efc850a8e384442a301
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-04-06 11:45:21 +0600

    Setting Tailwind config & added 2section in home page

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import { 'tailwindcss' }.config } */
+module.exports = {
+    content: [".src/**/ *.html', './ src/**/ *.js'"],
+    theme: {
+        extends: {},
+    },
+    plugins: [],
+}
